dbb411ef77ea8343588823b35548f3421ccc1762
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-21 17:07:46 -0400

    add in boilerplate auth/user/jwt functions and modify for whatdo user table

--- src/app.js
+++ src/app.js
@@ -4,28 +4,27 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const errorHandler = require('./middleware/error-handler');
+const authRouter = require('./auth/auth-router');
+const eventRouter = require('./event/event-router');
+const userRouter = require('./user/user-router');
 
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use('/api/auth', authRouter);
+app.use('/api/event', eventRouter);
+app.use('/api/user', userRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development'
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://whatdo@localhost/whatdo',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    'postgresql://whatdo@localhost/whatdo_test',
+  JWT_SECRET: process.env.JWT_SECRET || 'whatdo-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
 };
