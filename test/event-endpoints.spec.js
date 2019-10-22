cea88080943e1b57f5f8222eddcbb3359c86d291
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-22 11:02:22 -0400

    add event endpoint test file

---
+++ test/event-endpoints.spec.js
@@ -0,0 +1,90 @@
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+
+describe('Event Endpoints', function() {
+  let db;
+
+  const testUsers = helpers.makeUsersArray();
+  const [testUser] = testUsers;
+  const testEvents = helpers.makeEvents(testUser);
+
+  before('make knex instance', () => {
+    db = helpers.makeKnexInstance();
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('cleanup', () => helpers.cleanTables(db));
+
+  afterEach('cleanup', () => helpers.cleanTables(db));
+
+  describe(`GET /api/event`, () => {
+    beforeEach('insert users and events', () => {
+      return helpers.seedUsersEvents(db, testUsers, testEvents);
+    });
+
+    it(`responds with 200 and user's events`, () => {
+      return supertest(app)
+        .get(`/api/event`)
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .expect(200)
+        .expect(res => {
+          expect(res.body).to.have.keys('events');
+
+          testEvents.forEach((userEvent, idx) => {
+            const event = res.body.events[idx];
+            expect(event).to.have.property('id', userEvent.id);
+            expect(event).to.have.property('name', userEvent.name);
+            expect(event).to.have.property('fetch_id', userEvent.fetch_id);
+            expect(event).to.have.property('user_id', userEvent.user_id);
+          });
+        });
+    });
+  });
+
+  describe(`POST /api/event`, () => {
+    beforeEach('insert users and events', () => {
+      return helpers.seedUsersEvents(db, testUsers, testEvents);
+    });
+
+    const requiredFields = ['name', 'fetch_id'];
+
+    requiredFields.forEach(field => {
+      const newEvent = {
+        name: 'Test new event',
+        fetch_id: 12345
+      };
+
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newEvent[field];
+
+        return supertest(app)
+          .post('/api/event')
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .send(newEvent)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          });
+      });
+    });
+
+    it(`responds with 201 and created post when fields are correct`, () => {
+      const correctPostBody = {
+        name: 'New Event',
+        fetch_id: 12345
+      };
+      return supertest(app)
+        .post(`/api/event`)
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .send(correctPostBody)
+        .expect(201)
+        .expect({
+          id: testEvents.length + 1,
+          name: correctPostBody.name,
+          fetch_id: correctPostBody.fetch_id,
+          user_id: testUser.id
+        });
+    });
+  });
+});
