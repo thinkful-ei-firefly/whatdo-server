dfa37fd4fa426adf21992ea42060f3a5ad64e3e9
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-25 11:28:56 -0400

    Change DATE to TIMESTAMP and add seeds

--- migrations/004.do.alter_event_fields.sql
+++ migrations/004.do.alter_event_fields.sql
@@ -1,7 +1,7 @@
 ALTER TABLE event
       ADD COLUMN description TEXT,
-      ADD COLUMN start_time DATE NOT NULL,
-      ADD COLUMN stop_time DATE NOT NULL,
+      ADD COLUMN start_time TIMESTAMP NOT NULL,
+      ADD COLUMN stop_time TIMESTAMP NOT NULL,
       ADD COLUMN address TEXT NOT NULL,
       ADD COLUMN city_name TEXT NOT NULL,
       ADD COLUMN region_name TEXT NOT NULL,
--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
 TRUNCATE
-  "event",
+  event,
   "user";
 
-INSERT INTO "user" ("id", "username", "nickname", "email", "password")
+INSERT INTO "user" (id, username, nickname, email, password)
 VALUES
   (
     1,
@@ -15,28 +15,70 @@ VALUES
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
-INSERT INTO "event" ("id", "name", "fetch_id", "user_id")
+INSERT INTO event
+       (
+        id,
+        name,
+        fetch_id,
+        user_id,
+        description,
+        start_time,
+        stop_time,
+        address,
+        city_name,
+        region_name,
+        venue,
+        image,
+        url
+       )
 VALUES
   (
     1,
     'The Best Event',
     1234,
-    1
+    1,
+    'Short Description',
+    '1999-01-08 04:05:06',
+    '1999-01-08 06:05:06',
+    '123 Main St',
+    'Gotham',
+    'Northeast',
+    'Music hall',
+    'shorturl.at/ajIMQ',
+    'https://www.visitphilly.com/articles/philadelphia/fall-harvest-festivals/'
   ),
   (
     2,
     'A Better Event',
     1235,
-    1
+    1,
+    'Short Description',
+    '1999-01-08 04:05:06',
+    '1999-01-08 06:05:06',
+    '123 Main St',
+    'Gotham',
+    'Northeast',
+    'Music hall',
+    'shorturl.at/ajIMQ',
+    'https://www.visitphilly.com/articles/philadelphia/fall-harvest-festivals/'
   ),
   (
     3,
     'A Good Enough Event',
     1236,
-    1
+    1,
+    'Short Description',
+    '1999-01-08 04:05:06',
+    '1999-01-08 06:05:06',
+    '123 Main St',
+    'Gotham',
+    'Northeast',
+    'Music hall',
+    'shorturl.at/ajIMQ',
+    'https://www.visitphilly.com/articles/philadelphia/fall-harvest-festivals/'
   );
   
-SELECT setval('event_id_seq', (SELECT MAX(id) from "event"));
+SELECT setval('event_id_seq', (SELECT MAX(id) from event));
 SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
 
 COMMIT;
