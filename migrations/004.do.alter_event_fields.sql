7ec77a6cfe44dd0ba4096ab0f6da7b47035f9d8b
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-25 11:03:39 -0400

    Add fields from 3rd party API to event table

---
+++ migrations/004.do.alter_event_fields.sql
@@ -0,0 +1,10 @@
+ALTER TABLE "event"
+      ADD COLUMN description TEXT,
+      ADD COLUMN start_time DATE NOT NULL,
+      ADD COLUMN stop_time DATE NOT NULL,
+      ADD COLUMN address TEXT NOT NULL,
+      ADD COLUMN city_name TEXT NOT NULL,
+      ADD COLUMN region_name TEXT NOT NULL,
+      ADD COLUMN venue TEXT NOT NULL,
+      ADD COLUMN image TEXT,
+      ADD COLUMN url TEXT;
