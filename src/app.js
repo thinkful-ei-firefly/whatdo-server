59244018f2dcdc4a5de769872f34e8b573761a98
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-10-28 12:08:19 -0400

    change cors for dev

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
+const { NODE_ENV } = require('./config');
 const errorHandler = require('./middleware/error-handler');
 const authRouter = require('./auth/auth-router');
 const eventRouter = require('./event/event-router');
@@ -14,7 +14,8 @@ const app = express();
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(cors());
+
 app.use(helmet());
 
 app.get('/', (req, res) => {
